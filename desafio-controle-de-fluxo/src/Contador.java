e08470f203a70b518b4e865c51618f8a24c07c16
VituuDias <vitorgabriel30001@gmail.com>
2025-04-11 10:06:52 -0300

    Desafio contador, trabalhando controle de fluxo e tratamento de exceções

---
+++ desafio-controle-de-fluxo/src/Contador.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
+
+        int parametroUm = terminal.nextInt();
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (Exception e) {
+           System.err.println("O segundo parametro deve ser maior que o primeiro");
+        }
+
+    }
+
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if(parametroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for(int i =1; i<=contagem; i++){
+            System.out.println("Imprimindo o número " +i);
+
+        }
+    }
+}
